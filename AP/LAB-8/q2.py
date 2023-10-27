1ec169577d30ed9b03a3d345cbc7bde14389538e
AbhiramChitta <96126712+AbhiramChitta@users.noreply.github.com>
2023-10-27 15:34:21 +0530

    q2.py

--- AP/LAB-8/q2.py
+++ AP/LAB-8/q2.py
@@ -1,32 +1,87 @@
 from tkinter import *
-k = Tk()
-result = StringVar()
-button1 = Button(k, text="1", width=5, height=2)
-button2 = Button(k, text="2", width=5, height=2)
-button3 = Button(k, text="3", width=5, height=2)
-button4 = Button(k, text="4", width=5, height=2)
-button5 = Button(k, text="5", width=5, height=2)
-button6 = Button(k, text="6", width=5, height=2)
-button7 = Button(k, text="7", width=5, height=2)
-button8 = Button(k, text="8", width=5, height=2)
-button9 = Button(k, text="9", width=5, height=2)
-button0 = Button(k, text="0", width=5, height=2)
-button11 = Button(k, text="*", width=5, height=2)
-button12 = Button(k, text="/", width=5, height=2)
-button13 = Button(k, text="-", width=5, height=2)
-button14 = Button(k, text="+", width=5, height=2)
-button15 = Button(k, text="=", width=5, height=2)
-button16 = Button(k, text=".", width=5, height=2)
-button1.grid(row=1, column=0, padx=5, pady=5)
-button2.grid(row=1, column=1, padx=5, pady=5)
-button3.grid(row=1, column=2, padx=5, pady=5)
-button4.grid(row=2, column=0, padx=5, pady=5)
-button5.grid(row=2, column=1, padx=5, pady=5)
-button6.grid(row=2, column=2, padx=5, pady=5)
-button7.grid(row=3, column=0, padx=5, pady=5)
-button8.grid(row=3, column=1, padx=5, pady=5)
-button9.grid(row=3, column=2, padx=5, pady=5)
-button0.grid(row=4, column=1, padx=5, pady=5)
-entry = Entry(k, textvariable=result )
-entry.grid(row=0, column=0, columnspan=4)
-k.mainloop()
+
+calc = Tk()
+calc.geometry("300x300")
+calc.title("Calculator")
+calcLabel = Label(calc, text="CALC", bg='White',font=("Times New Roman", 30, 'normal'))
+calcLabel.pack(side=TOP)
+calc.config(background='black')
+
+textin = StringVar()
+operator = ""
+
+
+def select(number):  # lambda:clickbut(1)
+    global operator
+    operator = operator+str(number)
+    textin.set(operator)
+
+
+def equals():
+    global operator
+    add = str(eval(operator))
+    textin.set(add)
+    operator = ''
+
+
+def clear():
+    textin.set('')
+
+
+metext = Entry(calc, font=("Courier New", 20, 'bold'), textvar=textin, width=20, bd=5, bg='white')
+metext.pack()
+
+button1 = Button(calc, bg='white', font =("Times New Roman", 25, 'bold'), command=lambda: select(1), text="1")
+button1.place(x=40, y=100)
+
+button2 = Button(calc, bg='white', font =("Times New Roman", 25, 'bold'), command=lambda: select(2), text="2")
+button2.place(x=40, y=150)
+
+button3 = Button(calc, bg='white', font =("Times New Roman", 25, 'bold'), command=lambda: select(3), text="3")
+button3.place(x=40, y=200)
+
+button4 = Button(calc, bg='white', font =("Times New Roman", 25, 'bold'), command=lambda: select(4), text="4")
+button4.place(x=80, y=100)
+
+button5 = Button(calc, bg='white', font =("Times New Roman", 25, 'bold'), command=lambda: select(5), text="5")
+button5.place(x=80, y=150)
+
+button6 = Button(calc, bg='white', font =("Times New Roman", 25, 'bold'), command=lambda: select(6), text="6")
+button6.place(x=80, y=200)
+
+button7 = Button(calc, bg='white', font =("Times New Roman", 25, 'bold'), command=lambda: select(7), text="7")
+button7.place(x=120, y=100)
+
+button8 = Button(calc, bg='white', font =("Times New Roman", 25, 'bold'), command=lambda: select(8), text="8")
+button8.place(x=120, y=150)
+
+button9 = Button(calc, bg='white', font =("Times New Roman", 25, 'bold'), command=lambda: select(9), text="9")
+button9.place(x=120, y=200)
+
+button0 = Button(calc, bg='white', font =("Times New Roman", 25, 'bold'), command=lambda: select(0), text="0")
+button0.place(x=40, y=250)
+
+buttondot = Button(calc, bg='white', font =("Courier New", 20, 'bold'), command=lambda: select("."), text=".")
+buttondot.place(x=80, y=250)
+
+buttonadd = Button(calc, bg='white', text="+", font =("Courier New", 20, 'bold'), command=lambda: select("+"))
+buttonadd.place(x=160, y=100)
+
+buttonsub = Button(calc, bg='white', text="-", font =("Courier New", 20, 'bold'), command=lambda: select("-"))
+buttonsub.place(x=160, y=150)
+
+buttonmul = Button(calc, bg='white', text="*", font =("Courier New", 20, 'bold'), command=lambda: select("*"))
+buttonmul.place(x=160, y=200)
+
+buttondiv = Button(calc, bg='white', text="/", font =("Courier New", 20, 'bold'), command=lambda: select("/"))
+buttondiv.place(x=160, y=250)
+
+buttonmod = Button(calc, bg='white', font =("Courier New", 20, 'bold'), command=lambda: select("%"), text="%")
+buttonmod.place(x=120, y=250)
+
+buttonclear = Button(calc, bg='white', font =("Courier New", 20, 'bold'), text="CE", command=clear)
+buttonclear.place(x=200, y=250)
+
+buttonequal = Button(calc, bg='white', font =("Courier New", 20, 'bold'), command=equals, text="=")
+buttonequal.place(x=200, y=196)
+calc.mainloop()
